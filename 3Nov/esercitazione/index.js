05ea117df3c26cb4eb7eba2ddf4dde0fe5b986e2
silvia.hth <silvia.hth@gmail.com>
2021-11-04 10:06:38 +0100

    esercizio corretto

--- 3Nov/esercitazione/index.js
+++ 3Nov/esercitazione/index.js
@@ -1,38 +1,46 @@
-const q =(selector) => document.querySelector(selector);
-const btn = document.querySelector('button');
+const q = (selector) => document.querySelector(selector);
+
 const render = (container, items) => {
-    const elements =items.map((element)=>{
-        if (element.completed === true) {    
-            return `<li>
-                <label for="search"><input type="checkbox" checked= true/>
-                <p>${element.title}</p> </label>
-            </li>`}
-        else {
-            return `<li>
-                <label for="search"><input type="checkbox"/>
-                <p>${element.title}</p> </label>
-            </li>`
-        }
+    const elements = items.map((element) => {
+        // if (element.completed) {
+        //     return `<li>
+        //         <label for="search"><input type="checkbox" checked= true/>
+        //         <p>${element.title}</p> </label>
+        //     </li>`}
+        // else {
+        //     return `<li>
+        //         <label for="search"><input type="checkbox"/>
+        //         <p>${element.title}</p> </label>
+        //     </li>`
+        // }
+        return `<li>
+                 <label for="search"><input type="checkbox" ${element.completed ? 'checked' : ''} />
+                 <p>${element.title}</p> </label>
+                </li>`
 
 
-        });
-        
-        
+    });
 
-        btn.addEventListener("click", () => {
-            elements.pop('');
-            const content = elements.join('');
-            container.innerHTML= content;
-        });
 
-        const content = elements.join('');
-        container.innerHTML= content;
-        
-       
- };
+    const content = elements.join('');
+    container.innerHTML = content;
 
-document.addEventListener( 'DOMContentLoaded', () => {
+
+};
+
+document.addEventListener('DOMContentLoaded', () => {
     const list = q('ul');
     render(list, data);
+    const btn = q('button');
+
+    btn.addEventListener("click", () => {
+        data.pop('');
+        render(list, data);
+    });
 
 });
+
+
+
+
+
